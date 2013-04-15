64222a5cf98555632e3b11755ae06ec4f9731478
latrokles <latrokles@gmail.com>
2013-04-14 20:04:37 -0700

    Added steps to install barebone command line tools and update homebrew

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,9 +5,33 @@
 #
 
 # determine what OS are we on, osx (darwin) or ubuntu (linux-gnu)
+SL_GCC_INSTALLER='GCC-10.6.pkg'
+SL_GCC_INSTALLER_URL='https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.6.pkg'
+SL_GCC_INSTALLER_CHECKSUM='650cc32d89beb50841696aeb37a4c2d5'
 
 osx(){
   echo "bootstrapping osx workstation"
+  echo "installing osx gcc installer..."
+  \curl ${SL_GCC_INSTALLER_URL} -o ~/Downloads/${SL_GCC_INSTALLER}
+  if [ "${SL_GCC_INSTALLER_CHECKSUM}" = "`md5 ~/Downloads/${SL_GCC_INSTALLER} | cut -d= -f2 | sed -e 's/[ ]//g'`" ]
+  then
+    sudo installer -pkg ~/Downloads/${SL_GCC_INSTALLER}  -target /
+    rm ~/Downloads/${SL_GCC_INSTALLER}
+  else
+    echo "installer doesn't match expected checksum. Exiting..."
+    exit 1
+  fi 
+
+  echo "installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+  echo "installing git..."
+  brew install git
+
+  echo "upgrading homebrew..."
+  brew update
+  brew upgrade
+
   echo "installing ruby and rvm..."
   \curl -L https://get.rvm.io | bash -s stable --ruby
 
@@ -16,7 +40,6 @@ osx(){
 
   echo "installing chef..."
   sudo gem install chef --no-ri --no-rdoc
-
   echo "finished bootstrapping osx!"
 }
 
